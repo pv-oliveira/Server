9eefd1b1d2cfae4c3cfd8dc424a0ef9f7533c77b
Paulo Victor <paulo.victor.ig@hotmail.com>
2022-02-08 20:22:04 -0300

    commit port2

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 
 
 app.get('/',(req,res)=>{
-	// res.send(database.users);
+	res.send('está funcionando');
 })
 
 app.post('/signin',(req, res)=> { signin.handleSignin(req, res, db, bcrypt)})
